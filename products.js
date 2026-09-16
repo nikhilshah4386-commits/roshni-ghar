317ef11f4e10994a97461b03c88ef288a692b5c5
Nikhil Shah <nikhilshah4386@gmail.com>
2026-09-16 12:45:04 +0530

    Add product catalog data system

---
+++ products.js
@@ -0,0 +1,63 @@
+const productCatalog = {
+  Lights: [
+    {
+      name: "Modern Ceiling Light",
+      image: "",
+      description: "Roshni Ghar Product"
+    },
+    {
+      name: "LED Panel Light",
+      image: "",
+      description: "Roshni Ghar Product"
+    },
+    {
+      name: "Decorative Wall Light",
+      image: "",
+      description: "Roshni Ghar Product"
+    },
+    {
+      name: "Smart Ceiling Light",
+      image: "",
+      description: "Roshni Ghar Product"
+    }
+  ],
+
+  Chandeliers: [
+    {
+      name: "Luxury Chandelier",
+      image: "",
+      description: "Roshni Ghar Product"
+    },
+    {
+      name: "Modern Crystal Chandelier",
+      image: "",
+      description: "Roshni Ghar Product"
+    }
+  ],
+
+  Fans: [
+    {
+      name: "Modern Ceiling Fan",
+      image: "",
+      description: "Roshni Ghar Product"
+    },
+    {
+      name: "Smart Fan",
+      image: "",
+      description: "Roshni Ghar Product"
+    }
+  ],
+
+  "Interior Solutions": [
+    {
+      name: "Interior Lighting",
+      image: "",
+      description: "Roshni Ghar Product"
+    },
+    {
+      name: "Living Room Lighting",
+      image: "",
+      description: "Roshni Ghar Product"
+    }
+  ]
+};
